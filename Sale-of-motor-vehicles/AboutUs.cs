74f513119ed17fe404a087821e1aaa9a25fe60d9
dima20004 <User@WIN-6N0AUP9GUP7>
2022-11-07 19:46:02 +0300

    Добавленные формы

---
+++ Sale-of-motor-vehicles/AboutUs.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sale_of_motor_vehicles
+{
+    public partial class AboutUs : Form
+    {
+        public AboutUs()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Главная glaw = new Главная();
+            glaw.Show();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Мои_Анкеты mi = new Мои_Анкеты();
+            mi.Show();
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Покупки pop = new Покупки();
+            pop.Show();
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AboutUs pop = new AboutUs();
+            pop.Show();
+        }
+    }
+}
---
+++ Sale-of-motor-vehicles/Покупки.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sale_of_motor_vehicles
+{
+    public partial class Покупки : Form
+    {
+        public Покупки()
+        {
+            InitializeComponent();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Главная glaw = new Главная();
+            glaw.Show();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Мои_Анкеты mi = new Мои_Анкеты();
+            mi.Show();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Покупки pop = new Покупки();
+            pop.Show();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AboutUs ab = new AboutUs();
+            ab.Show();
+        }
+    }
+}
